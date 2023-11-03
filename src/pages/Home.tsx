6a13055d6d7896ec6b8ab11e2d779f6252cd73d9
vivekkum-ar <vivekkr9119@gmail.com>
2023-11-03 12:59:37 +0530

    Add Firebase analytics and authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,25 @@
 import '../Layout.css'
 import { useFormData } from '../FormDataProvider';
 import { useNavigate } from 'react-router-dom';
-
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect } from 'react';
 
 function Home() {
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        // ...
+      } else {
+        // User is signed out
+        navigate('/login');
+      }
+    });
+  }, []);
+  
   const { formData, setFormData } = useFormData();
   const navigate = useNavigate();
 
--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,34 +1,68 @@
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import React, { useState } from 'react'
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import {auth} from "../../firebase";
+import AlertError from '../../components/alertError';
+import AlertSuccess from '../../components/alertSuccess';
+import { useNavigate } from 'react-router-dom';
 
 interface LoginProps {
   // Add your prop types here
 }
 
 const Login: React.FC<LoginProps> = ({}) => {
+  const navigate = useNavigate();
     const [Credentials, setCredentials] = useState({
-        email: '',
-        password: ''
+        email: 'email',
+        password: 'password'
     });
 
-    const auth = getAuth();
-signInWithEmailAndPassword(auth, Credentials.email, Credentials.password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log(user);
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+    /* ------------------- Used same state for error handling and success message ------------------- */
+   const [Error, setError] = useState({
+        errorType: '',
+        errorMsg: ''
+    });
+    
+    const Login = async (e:any) => {
+      e.preventDefault();
+      // const auth = getAuth();
+      signInWithEmailAndPassword(auth, Credentials.email, Credentials.password)
+        .then((userCredential) => {
+          // Signed in 
+          setError({errorType:"Success",errorMsg:"Login Successfull"});
+          const user = userCredential.user; 
+          navigate("/home");
+          console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorType = error.code.toString();
+          const errorMsg = error.message;
+          // console.log(errorCode, errorMessage);
+          setError({errorType,errorMsg});
+        });
+    }
   return (
+    <>
+     <div className="flex items-center justify-center min-h-screen">
+      <div className="">
+    <h1 className="text-5xl mb-5 font-bold text-gray-800">
+      M/S Jaya Enterprises
+    </h1>
+
+    {/* {Error.errorType == "" ? "" : <AlertError errorCode={Error.errorType} error={Error.errorMsg}/>} */}
+    {(Error.errorType == "") ? "" : (Error.errorType != "Success") ? <AlertError errorCode={Error.errorType} error={Error.errorMsg}/> : ""}
+    {(Error.errorType == "Success") ? <AlertSuccess msgCode={Error.errorType} message={Error.errorMsg}/> : ""}
     <form className="max-w-sm mx-auto"> 
-  <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-  <input type="email" id="email" aria-describedby="helper-text-explanation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" />
-  <p id="helper-text-explanation" className="mt-2 text-sm text-gray-500 dark:text-gray-400">We’ll never share your details. Read our <a href="#" className="font-medium text-blue-600 hover:underline dark:text-blue-500">Privacy Policy</a>.</p>
+  <label htmlFor="email" className="block mb-2 text-start text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+  <input type="email" required id="email" onChange={(e) => {setCredentials({...Credentials,email:e.target.value})}} aria-describedby="helper-text-explanation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" />
+  {/* <p id="helper-text-explanation" className="mt-2 text-sm text-start text-gray-500 dark:text-gray-400">We’ll never share your details. Read our <a href="#" className="font-medium text-blue-600 hover:underline dark:text-blue-500">Privacy Policy</a>.</p> */}
+  <label htmlFor="password" className="block mt-5 mb-2 text-start text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+  <input type="password" required id="password" onChange={(e) => {setCredentials({...Credentials,password:e.target.value})}} aria-describedby="helper-text-explanation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="password" />
+  {/* <p id="helper-text-explanation" className="mt-2 text-sm text-start text-gray-500 dark:text-gray-400">We’ll never share your details. Read our <a href="#" className="font-medium text-blue-600 hover:underline dark:text-blue-500">Privacy Policy</a>.</p> */}
+  <button type="button" onClick={(e) => {Login(e)}} className="mt-5 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Login</button>
 </form>
+</div></div>
+</>
   )
 }
 
